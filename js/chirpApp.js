08850945d8abbba968fe8bf975ec3c94efed7d6b
Luis Correa <yancel209@gmail.com>
2021-02-10 15:57:39 -0500

    subiendo emprender por internet

--- js/chirpApp.js
+++ js/chirpApp.js
@@ -53,11 +53,47 @@ app.config(function($routeProvider){
         }).when('/agencia_emprendedores', {
                 templateUrl: 'template/agencia_emprendedores.html',
                 controller: 'agenciaEmprendedoresController'
+        }).when('/emprender', {
+                templateUrl: 'template/emprender_internet.html',
+                controller: 'emprenderInternetController'
         })
 });
 
 app.factory('postService', function($resource){
 	return $resource('/api/posts/:id');
+});
+
+
+app.controller('emprenderInternetController', function(postService, $scope, $rootScope){
+	$scope.sesion = sesionjs;
+        $scope.menu = menujs;
+        $scope.contactos = contactosjs;
+        $scope.tecnologia = []
+        $scope.proyectos = servicio_comunitario_luis;
+        $scope.proyectos_realizados_luis = proyectos_realizados_luis;
+        $scope.trabajo_grado_luis = trabajo_grado_luis;
+        $scope.obtenerDetalleTrabajoGrado = function(id){
+                console.log('obtenerDetalleTrabajoGrado')
+                var proyecto = {};
+                for(let proyecto2 of $scope.trabajo_grado_luis){
+                        if(proyecto2.id == id){
+                                proyecto = proyecto2;
+                        }
+                }
+                $scope.tecnologia = proyecto.tecnologia
+                
+        };
+        $scope.obtenerDetalle = function(id){
+                console.log('obtenerDetalle')
+                var proyecto = {};
+                for(let proyecto2 of $scope.proyectos_realizados_luis){
+                        if(proyecto2.id == id){
+                                proyecto = proyecto2;
+                        }
+                }
+                $scope.tecnologia = proyecto.tecnologia
+                
+        };
 });
 
 app.controller('agenciaEmpresasController', function(postService, $scope, $rootScope){
